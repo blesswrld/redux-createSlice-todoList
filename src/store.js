0e05aac4495a319f9c9f992e3918ad698c485246
blesswrld <roninfortnite71@gmail.com>
2025-04-02 23:48:24 +0300

    use method createSlice

---
+++ src/store.js
@@ -0,0 +1,34 @@
+import { createStore } from "redux";
+import { nanoid, createSlice } from "@reduxjs/toolkit";
+
+const todoSlice = createSlice({
+    name: "@@todos",
+    initialState: [],
+    reducers: {
+        addTodo: {
+            reducer: (state, action) => {
+                state.push(action.payload);
+            },
+            prepare: (title) => ({
+                payload: {
+                    title,
+                    id: nanoid(),
+                    completed: false,
+                },
+            }),
+        },
+        removeTodo: (state, action) => {
+            const id = action.payload;
+            return state.filter((todo) => todo.id !== id);
+        },
+        toggleTodo: (state, action) => {
+            const id = action.payload;
+            const todo = state.find((todo) => todo.id === id);
+            todo.completed = !todo.completed;
+        },
+    },
+});
+
+export const { addTodo, removeTodo, toggleTodo } = todoSlice.actions;
+
+export const store = createStore(todoSlice.reducer);
